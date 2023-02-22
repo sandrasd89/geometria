8bea67fdbfc94ae8dccdebbc0f4effcc97599073
Sandra Sáez Ibarra <sansaeiba@alu.edu.gva.es>
2023-02-22 19:59:33 +0100

    documentar es de sabios, aunque leerla sea pesado

---
+++ ladocu/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Circulo_SSI"},{"p":"<Unnamed>","l":"FiguraGeometrica_SSI"},{"p":"<Unnamed>","l":"Rectangulo_SSI"},{"p":"<Unnamed>","l":"Triangulo_SSI"}];updateSearchResults();
