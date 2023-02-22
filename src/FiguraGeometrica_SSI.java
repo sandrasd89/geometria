0d0fed6966b951f3318a4787a3a1e8d986c8be32
Sandra Sáez Ibarra <sansaeiba@alu.edu.gva.es>
2023-02-22 19:32:54 +0100

    versión refactorizada

--- src/FiguraGeometrica_SSI.java
+++ src/FiguraGeometrica_SSI.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_SSI {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_SSI(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
