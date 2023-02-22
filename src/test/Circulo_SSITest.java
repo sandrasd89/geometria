73ebee7e3e95a9b58b803518ebc5661cd766a2a4
Sandra Sáez Ibarra <sansaeiba@alu.edu.gva.es>
2023-02-22 20:47:49 +0100

    Uff ahora sí

---
+++ src/test/Circulo_SSITest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Circulo_SSITest {
+
+    @org.junit.jupiter.api.Test
+    void area() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void perimetro() {
+    }
+}
